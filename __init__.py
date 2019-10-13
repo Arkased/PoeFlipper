07a419038218b04e8d87c4aeb98353dc9ccc4f7e
Kevin Liu <kevintmliu@gmail.com>
2019-10-13 14:57:33 -0700

    calculations first implementation (buggy)

--- __init__.py
+++ __init__.py
@@ -4,3 +4,20 @@ Created on Wed Oct  2 16:39:54 2019
 
 @author: kevin
 """
+
+import utils
+
+divs = utils.div_cards();
+divs = filter(utils.price_pred, divs)
+divs = filter(utils.name_pred, divs)
+divs = utils.trim_dics(divs)
+
+for div in divs:
+    div['investment'] = div['mean'] * div['stackSize']
+    div['returnId'] = utils.ID_DIC[div['name']]
+    div['return'] = utils.PRICES[div['returnId']]
+    div['profit'] = (div['investment'] - div['return'])
+    div['profitPerTrade'] = div['profit'] / (div['stackSize'] + 1)
+    div['yield'] = 1 + div['profit'] / div['investment']
+
+divs.sort(key=lambda d: d['profitPerTrade'] * d['yield'])
--- utils.py
+++ utils.py
@@ -5,6 +5,16 @@ API_URL = 'http://api.poe.watch/'
 LEAGUE = 'Blight'
 
 
+def load_id_dic():
+    with open('item_ids.csv', 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        id_dic = {row[0]: int(row[1]) for row in reader}
+    return id_dic
+
+
+ID_DIC = load_id_dic()
+
+
 def save_dicts(dics: list):
     """Saves a list of dictionaries DICS as a csv file."""
     with open('out.csv', 'w', newline='') as f:
@@ -17,7 +27,7 @@ def save_dicts(dics: list):
 def api(func, params):
     """Attempts to access the poe.watch API with function string FUNC and parameters dictionary PARAMS, returning the
     JSON value converted to python """
-    print('calling', params)
+    print('calling', func, params)
     result = requests.get(API_URL + func, params=params)
     try:
         return result.json()
@@ -27,7 +37,7 @@ def api(func, params):
 
 def item_price(id):
     """Looks up the mean price of item ID"""
-    return api('item', {'id', id})['mean']
+    return api('itemhistory', {'id': id, 'league': LEAGUE})['mean']
 
 
 def div_cards():
@@ -49,7 +59,7 @@ def trim_dics(dics):
 SELECTED_CARDS = open('div_cards.txt', 'r').read().splitlines()
 
 
-def price_pred(dic, floor, ceil):
+def price_pred(dic, floor=40, ceil=2000):
     """Test if investment of item DIC is within range FLOOR and CEIL"""
     try:
         investment = dic['mean'] * dic['stackSize']
