80e40c52cc718be9902649cd0292ecd17c456966
Kevin Liu <kevintmliu@gmail.com>
2019-11-23 12:30:08 -0800

    working  version of both, need to add new item ids

--- __init__.py
+++ __init__.py
@@ -7,13 +7,15 @@ Created on Wed Oct  2 16:39:54 2019
 
 import utils
 
+utils.set_api_poe_ninja()
 divs = utils.div_cards()
 divs = filter(utils.price_pred, divs)
 divs = filter(utils.name_pred, divs)
 divs = utils.trim_dics(divs)
 
 for div in divs:
-    div['investment'] = div['mean'] * div['stackSize']
+    print(len(utils.ITEM_DATA))
+    div['investment'] = div[utils.MEAN_NAME] * div['stackSize']
     div['returnId'] = utils.ID_DIC[div['name']]
     div['return'] = utils.lookup_price(div['returnId'])
     div['profit'] = (div['return'] - div['investment'])
--- utils.py
+++ utils.py
@@ -1,8 +1,11 @@
 import csv
 import requests
 
-API_URL = 'http://api.poe.watch/'
 LEAGUE = 'Blight'
+div_cards = ''
+all_data = ''
+ITEM_DATA = ''
+MEAN_NAME = ''
 
 
 def load_id_dic():
@@ -24,45 +27,106 @@ def save_dicts(dics: list):
             wtr.writerow(dic.values())
 
 
-def api(func, params):
+# API-specific functions
+
+def _poe_watch_api(func, params):
     """Attempts to access the poe.watch API with function string FUNC and parameters dictionary PARAMS, returning the
     JSON value converted to python """
-    print('calling', func, params)
-    result = requests.get(API_URL + func, params=params)
+    api_url = 'http://api.poe.watch/'
+    print('calling', func, params, 'to', api_url)
+    result = requests.get(api_url + func, params=params)
     try:
         return result.json()
     except:
         print(result.status_code)
 
 
-ITEM_DATA = api('compact', {'league': LEAGUE})
+def _all_data_poe_watch():
+    return _poe_watch_api('compact', {'league': LEAGUE})
 
 
-def lookup_price(target_id, lower=0, upper=len(ITEM_DATA) - 1):
-    """Uses binary search to look up the mean price of item ID"""
-    i = (lower + upper) // 2  # the midpoint of the two bounds
-    current_id = ITEM_DATA[i]['id']  # id of the midpoint
-    if current_id == target_id:
-        return ITEM_DATA[i]['mean']
-    elif lower != upper:
-        if current_id < target_id:
-            return lookup_price(target_id, i, upper)
-        elif current_id > target_id:
-            return lookup_price(target_id, lower, i)
-
-    raise ValueError('item id', id, 'not found')
-
-
-def div_cards():
+def _div_cards_poe_watch():
     """Returns data all data on all div cards as a list of dictionaries"""
-    return api('get', {'league': LEAGUE, 'category': 'card'})
+    return _poe_watch_api('get', {'league': LEAGUE, 'category': 'card'})
+
+
+def set_api_poe_watch():
+    """Sets the api from which to querry data to poe.watch"""
+    global div_cards
+    global all_data
+    global ITEM_DATA
+    global MEAN_NAME
+    div_cards = _div_cards_poe_watch
+    all_data = _all_data_poe_watch
+    ITEM_DATA = all_data()
+    MEAN_NAME = 'mean'
+
+
+def _poe_ninja_api(func, params):
+    """Attempts to access the poe.ninja API with function string FUNC and parameters dictionary PARAMS, returning the
+    JSON value converted to a python list"""
+    api_url = 'https://poe.ninja/api/Data/'
+    print('calling', func, params, 'to', api_url)
+    result = requests.get(api_url + func, params=params)
+    try:
+        return result.json()['lines']
+    except:
+        print(result.status_code)
+
+
+def _all_data_poe_ninja():
+    with open('poe_ninja_apis.csv', 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        funcs = [item for sublist in reader for item in sublist]
+    dics = []
+    for func in funcs:
+        dics.extend(_poe_ninja_api(func, {'league': LEAGUE}))
+    dics.sort(key=lambda dic: dic['id'])
+    return dics
+
+
+def _div_cards_poe_ninja():
+    return _poe_ninja_api('GetDivinationCardsOverview', {'league': LEAGUE})
+
+
+def set_api_poe_ninja():
+    """Sets the api from which to querry data to poe.ninja"""
+    global div_cards
+    global all_data
+    global ITEM_DATA
+    global MEAN_NAME
+    div_cards = _div_cards_poe_ninja
+    all_data = _all_data_poe_ninja
+    ITEM_DATA = all_data()
+    MEAN_NAME = 'chaosValue'
+
+
+# Misc. functions
+
+
+def lookup_price(target_id):
+    """Uses binary search to look up the mean price of item ID"""
+
+    def lookup_recursive(trgt_id, lower, upper):
+        i = (lower + upper) // 2  # the midpoint of the two bounds
+        current_id = ITEM_DATA[i]['id']  # id of the midpoint
+        if lower > upper:
+            raise ValueError('item id', id, 'not found')
+        if current_id == trgt_id:
+            return ITEM_DATA[i][MEAN_NAME]
+        elif current_id < trgt_id:
+            return lookup_recursive(trgt_id, i, upper)
+        elif current_id > trgt_id:
+            return lookup_recursive(trgt_id, lower, i)
+
+    return lookup_recursive(target_id, 0, len(ITEM_DATA) - 1)
 
 
 def trim_dics(dics):
     """Selects the name, stackSize, and mean entries of each entry of DICS"""
 
     def select(dic):
-        return {entry: dic[entry] for entry in ['name', 'stackSize', 'mean']}
+        return {entry: dic[entry] for entry in ['name', 'stackSize', MEAN_NAME]}
 
     return list(map(select, dics))
 
@@ -75,7 +139,7 @@ SELECTED_CARDS = open('div_cards.txt', 'r').read().splitlines()
 def price_pred(dic, floor=40, ceil=2000):
     """Test if investment of item DIC is within range FLOOR and CEIL"""
     try:
-        investment = dic['mean'] * dic['stackSize']
+        investment = dic[MEAN_NAME] * dic['stackSize']
         return floor <= investment <= ceil
     except KeyError:
         return False
